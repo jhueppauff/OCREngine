59f1764c88045a5e24253a3be4c9f180107b34b2
Julian Hüpppauff <julian@hueppauff.info>
2018-09-17 21:17:14 +0200

    added new model

---
+++ OCREngine.WebApi/PdfConverter/Files.cs
@@ -0,0 +1,11 @@
+﻿namespace OCREngine.WebApi.PdfConverter
+{
+    public class Files
+    {
+        public string FileName { get; set; }
+
+        public string FilePath { get; set; }
+
+        public Models.OcrResults OcrResultn { get; set; }
+    }
+}
