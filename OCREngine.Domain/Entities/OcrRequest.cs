e65fb7aba3ef415a228ad6ca12a65ec889757b98
Julian Hüpppauff <julian@hueppauff.info>
2018-11-18 21:47:42 +0100

    updated model

--- OCREngine.Domain/Entities/OcrRequest.cs
+++ OCREngine.Domain/Entities/OcrRequest.cs
@@ -1,10 +1,12 @@
 ﻿namespace OCREngine.Domain.Entities
 {
+    using Microsoft.WindowsAzure.Storage;
+    using Microsoft.WindowsAzure.Storage.Table;
     using OCREngine.Domain.Infrastructure;
     using System;
     using System.Collections.Generic;
 
-    public class OcrRequest : ValueObject
+    public class OcrRequest : ValueObject, ITableEntity
     {
         public Guid RequestId { get; set; }
 
@@ -12,7 +14,18 @@
 
         public DateTime FinishDate { get; set; }
 
+        public Uri BlobUri { get; set; }
+
         public ProcessingStates ProcessingState { get; set; }
+
+        public AggregateException ProcessingException { get; set; }
+
+        public string ExceptionMessage { get; set; }
+
+        public string PartitionKey { get; set; }
+        public string RowKey { get; set; }
+        public DateTimeOffset Timestamp { get; set; }
+        public string ETag { get; set; }
 
         private OcrRequest()
         {
@@ -25,6 +38,16 @@
         }
 
         protected override IEnumerable<object> GetAtomicValues()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ReadEntity(IDictionary<string, EntityProperty> properties, OperationContext operationContext)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDictionary<string, EntityProperty> WriteEntity(OperationContext operationContext)
         {
             throw new NotImplementedException();
         }
