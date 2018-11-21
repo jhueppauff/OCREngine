4ded21ddeafe259d4c5bfcb43ede32e778b6a91d
Julian Hüpppauff <julian@hueppauff.info>
2018-11-21 19:23:57 +0100

    added api key

---
+++ OCREngine.Domain/Entities/APIKey.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OCREngine.Domain.Entities
+{
+    public class APIKey : ITableEntity
+    {
+        public Guid Id { get; set; }
+
+        public Guid Key { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public string PartitionKey { get; set; }
+        public string RowKey { get; set; }
+
+        public string ETag { get; set; }
+    }
+}
