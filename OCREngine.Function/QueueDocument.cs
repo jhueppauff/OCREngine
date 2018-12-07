c74647ef0f6a14b76c68c7e21c3a1d390c4e05b3
Julian Hüppauff <julian@hueppauff.com>
2018-12-07 11:59:17 +0100

    added az function

---
+++ OCREngine.Function/QueueDocument.cs
@@ -0,0 +1,37 @@
+using System.IO;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Azure.WebJobs.Host;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace OCREngine.Function
+{
+    public static class QueueDocument
+    {
+        /// <summary>
+        /// Queues a new Document for Processing
+        /// </summary>
+        /// <param name="req"></param>
+        /// <param name="log"></param>
+        /// <returns></returns>
+        [FunctionName("QueueDocument")]
+        public static async Task<IActionResult> RunQueueDocument([HttpTrigger(AuthorizationLevel.Function, "post", Route = null)]HttpRequest req, NextId keyTable, CloudTable tableOut, TraceWriter log)
+        {
+            log.Info("Add new Document to queue started");
+
+            string name = req.Query["name"];
+
+            string requestBody = new StreamReader(req.Body).ReadToEnd();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            name = name ?? data?.name;
+
+            return name != null
+                ? (ActionResult)new OkObjectResult($"Hello, {name}")
+                : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+        }
+    }
+}
