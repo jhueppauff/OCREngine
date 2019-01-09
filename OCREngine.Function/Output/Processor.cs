12eefb8b62d2b785dd274c91b141308d49d53919
Julian Hüpppauff <julian@hueppauff.info>
2019-01-09 22:13:32 +0100

    initial add of processor

---
+++ OCREngine.Function/Output/Processor.cs
@@ -0,0 +1,16 @@
+﻿using OCREngine.Domain.Entities.Vision;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OCREngine.Function.Output
+{
+    public class Processor
+    {
+        public string BuildDocumentFromOcrResult(List<string> file, List<OcrResults> ocrResults)
+        {
+
+            return null;
+        }
+    }
+}
